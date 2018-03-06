d91399ab5cf57366780a3622653d189663f543e0
Jianyu <vyrians@gmail.com>
2018-03-06 17:58:48 +0800

    ++make_link.py

--- udemy/PythonTheNextLevel/graphs_networks/make_link.py
+++ udemy/PythonTheNextLevel/graphs_networks/make_link.py
@@ -9,3 +9,39 @@
 # What is a graph / network
 ## - Node
 ## - Edges
+
+# How to make a Graph/Network in Python?
+
+GRAPH = {}
+
+def make_link(Graph, node1, node2):
+  # A graph will be a dictionary containing nodes.
+  # These nodes will be a dictionary of neighbor nodes.
+  # The node's dict. key will be the neighbor node and its value will be 1.
+
+  # In other words, we insert values into dictionaries to model the graph.
+
+  # Check that nodes are registered in Graph
+  if node1 not in Graph:
+    Graph[node1] = {}
+  if node2 not in Graph:
+    Graph[node2] = {}
+
+  # Add information about the nodes themselves
+  (Graph[node1])[node2] = 1
+  (Graph[node2])[node1] = 1
+
+  return Graph
+
+if __name__ == '__main__':
+  flights = []
+  # Insert TUPLE values!
+  flights.append(('LAX','DFW'))
+  flights.append(('SAE','LAX'))
+  flights.append(('ORD','LAX'))
+  flights.append(('ORD','SAE'))
+  
+for (x,y) in flights:
+  print(make_link(GRAPH,x,y))
+
+
