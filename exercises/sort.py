daf7fd6e04058ac466b59be42722a82e9f94357b
Jianyu <vyrians@gmail.com>
2018-03-17 22:02:29 +0800

    ++sort.py

--- exercises/sort.py
+++ exercises/sort.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3.6
 
 # SORTS
-
-import sys
+# Python implementation of several sort algorithms
 
 class sort_class():
 
     def __init__(self, alist):
         self.tmplist = alist
+        self._RaiseEmptyList()
 
+    def _RaiseEmptyList(self):
+        if len(self.tmplist) == 0:
+            print('WARNING: List currently stored is empty.')
+
+    # Same as __init__(self), condense?
     def _NewList(self, alist):
-        if len(self.tmplist) > 0:
-            self.tmplist = alist
-        else:
-            print('ERROR: List currently stored is already empty.')
+        self.tmplist = alist
+        self._RaiseEmptyList()
 
     def _GetList(self):
         print('Original list:', self.tmplist) 
@@ -22,12 +25,9 @@ class sort_class():
         print(' *** INSERTION_SORT *** ')
         for i in reversed(range(1,len(self.tmplist))):
             for j in range(0,i):
-                # print('i = %s, j = %s' %(i, j))
                 if self.tmplist[j] > self.tmplist[j+1]:
                     key = self.tmplist.pop(j)
                     self.tmplist.insert(j+1, key)
-                # print('Modified list =', self.tmplist)
-            # print('Done with this inner loop.')
         return self.tmplist
 
     def _BubbleSort(self):
@@ -37,6 +37,7 @@ class sort_class():
         pass
 
 if __name__ == '__main__':
+     """
      list_A = [5,4,6,2,1,3]
      list_B = [22,91,2,64,87,31]
      x = sort_class(list_A)
@@ -45,3 +46,7 @@ if __name__ == '__main__':
      x._NewList(list_B)
      x._GetList()
      print(x._InsertionSort())
+     """
+
+     list_E = []
+     x = sort_class(list_E)
