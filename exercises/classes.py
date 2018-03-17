86fa6fd5b7f17c8be570d544a2168669fe5decdf
Jianyu <vyrians@gmail.com>
2018-03-17 11:50:28 +0800

    indentations now use 4 spaces, as per PEP8

--- exercises/classes.py
+++ exercises/classes.py
@@ -4,37 +4,37 @@
 
 class MyClass:
 
-  # Data attributes will OVERRIDE class methods
-  # Best to have a convention for class methods
-  # Maybe start method name with a _
+    # Data attributes will OVERRIDE class methods
+    # Best to have a convention for class methods
+    # Maybe start method name with a _
 
-  # this variable is shared by ALL instances of this class
-  # unbound_list = []
+    # this variable is shared by ALL instances of this class
+    # unbound_list = []
 
-  # Default initialization method
-  # Will be called when class is instantialized
-  def __init__(self, realpart, imagpart):
-    self.r = realpart
-    self.i = imagpart
+    # Default initialization method
+    # Will be called when class is instantialized
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
 
-  def f(self):
-    return 'Helloworld!'
+    def f(self):
+        return 'Helloworld!'
 
 class Reverse:
 
-  def __init__(self,data):
-    self.data = data
-    self.index = len(data)
+    def __init__(self,data):
+        self.data = data
+        self.index = len(data)
 
-  # Use __iter__(self) to return the object with a __next__(self) for iteration
-  def __iter__(self):
-    return self
+    # Use __iter__(self) to return the object with a __next__(self) for iteration
+    def __iter__(self):
+        return self
 
-  def __next__(self):
-    if self.index == 0:
-      raise StopIteration
-    self.index -= 1
-    return self.data[self.index]
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index -= 1
+        return self.data[self.index]
 
 # Functions only get one chance to return a result,
 # and thus must return all results AT ONCE.
@@ -46,21 +46,21 @@ class Reverse:
 
 # Generator, function uses YIELD instead of RETURN
 def reverse(data):
-  for index in range(len(data)-1, -1, -1):
-    yield data[index]
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
 
 if __name__ == '__main__':
 
-  y = reverse('spam')
-  print(next(y))
-  print(next(y))
-  print(next(y))
-  print(next(y))
-  print(next(y))
-  print(next(y))
+    y = reverse('spam')
+    print(next(y))
+    print(next(y))
+    print(next(y))
+    print(next(y))
+    print(next(y))
+    print(next(y))
 
-  # Iterators
-  #x = Reverse('spam')
-  #for char in (x):
-  #  print(char)
+    # Iterators
+    #x = Reverse('spam')
+    #for char in (x):
+    #  print(char)
 
--- exercises/dice_roll.py
+++ exercises/dice_roll.py
@@ -12,44 +12,44 @@ LAST_RESULT = []
 MAX_RECORD_LEN = 10
 
 def display_dice(min_roll, max_roll):
-  print('Simulating a', max_roll, 'sided dice ...')
-  print('MIN_ROLL =', min_roll, ' MAX_ROLL =', max_roll)
+    print('Simulating a', max_roll, 'sided dice ...')
+    print('MIN_ROLL =', min_roll, ' MAX_ROLL =', max_roll)
 
 def display_last():
-  print(LAST_RESULT)
+    print(LAST_RESULT)
   
 def display_average():
-  print('Last 10 average:',sum(LAST_RESULT)/float(len(LAST_RESULT)))
+    print('Last 10 average:',sum(LAST_RESULT)/float(len(LAST_RESULT)))
 
 def roll_dice(min_roll, max_roll):
-  global ROUND
-  ROUND = ROUND + 1 
-  result = random.randint(min_roll, max_roll)
-  print('ROUND', ROUND, '- rolled', result)
-  if len(LAST_RESULT) == MAX_RECORD_LEN:
-    LAST_RESULT.pop(0) # Remove first item from list
-  LAST_RESULT.append(result) # Append to the back of list
-  return result
+    global ROUND
+    ROUND = ROUND + 1 
+    result = random.randint(min_roll, max_roll)
+    print('ROUND', ROUND, '- rolled', result)
+    if len(LAST_RESULT) == MAX_RECORD_LEN:
+        LAST_RESULT.pop(0) # Remove first item from list
+    LAST_RESULT.append(result) # Append to the back of list
+    return result
 
 if __name__ == '__main__':
-  display_dice(MIN_ROLL, MAX_ROLL)
-  while True:
-    roll_dice(MIN_ROLL, MAX_ROLL)
-    display_last()
-    display_average()
+    display_dice(MIN_ROLL, MAX_ROLL)
+    while True:
+        roll_dice(MIN_ROLL, MAX_ROLL)
+        display_last()
+        display_average()
 
-    # For Python3+ raw_input() has been renamed to input()
-    user_input = input('Do you wish to continue? (Y/N) [Y] ')
+        # For Python3+ raw_input() has been renamed to input()
+        user_input = input('Do you wish to continue? (Y/N) [Y] ')
 
-    # If user input is empty, set to Y 
-    if not user_input:
-      user_input = 'Y'
+        # If user input is empty, set to Y 
+        if not user_input:
+            user_input = 'Y'
  
-    if user_input == 'N':
-      break
-    elif user_input != 'Y':
-      print('Invalid input, exiting.')
-      break
+        if user_input == 'N':
+            break
+        elif user_input != 'Y':
+            print('Invalid input, exiting.')
+            break
 
-  print('Execution complete!')
+    print('Execution complete!')
  
