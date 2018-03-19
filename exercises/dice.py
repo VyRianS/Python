7effa406cfccf13e4a006d7691cb22be15d3133a
Jianyu <vyrians@gmail.com>
2018-03-19 23:11:15 +0800

    Improvements to user prompt methods and main loop.

--- exercises/dice.py
+++ exercises/dice.py
@@ -7,29 +7,95 @@ import random
 class DiceClass():
 
     def __init__(self):
+        self.running = 1
         self.round_count = 0
         self.result_list = []
+        self.default_min_roll = 1
+        self.default_record_length = 10
 
-    def _SetState(self):
+    def _PromptMinRoll(self):
 
-        # Min roll, negative values are OK
-        self.min_roll = input('Lowest boundary: [1] ') or 1
-
-        # Check that max_roll is greater than min_roll
         while True:
-            self.max_roll = input('Highest boundary: [6] ') or 6
+            self.min_roll = input('Lowest boundary: ')
+            if not self.min_roll:
+                self.min_roll = self.default_min_roll
+                print('Default lowest boundary set to', self.default_min_roll)
+                return self.min_roll
+
+            # Ensure input is not a string
+            try:
+                int(self.min_roll)
+            except ValueError:
+                print('Invalid value, please re-input.')
+                continue
+
+            # Input is valid at this point
+            return self.min_roll
+
+    def _PromptMaxRoll(self):
+
+        while True:
+            self.max_roll = input('Highest boundary: ')
+            if not self.max_roll:
+                self.max_roll = int(self.min_roll) + 1
+                print('Default highest boundary set to', self.max_roll)
+                return self.max_roll
+
+            try:
+                int(self.max_roll)
+            except ValueError:
+                print('Invalid value, please re-input.')
+                continue
+
             if int(self.max_roll) <= int(self.min_roll):
-                print('Invalid value, too low.')
-            else:
-                break
+                print('Highest bound must be at least 1 greater than minimum.')
+                continue
 
-        # Check that length is not negative
+            # Input is valid at this point
+            return self.max_roll
+
+    def _PromptRecordLength(self):
+
         while True:
-            self.max_record_length = input('Number of rolls to record: [10] ') or 10
-            if int(self.max_record_length) <= 0:
-                print('At least 1 record must be kept.')
-            else:
-                break
+            self.record_length = input('Maximum records: ')
+            if not self.record_length:
+                self.record_length = self.default_record_length
+                print('Maximum record length set to', self.default_record_length)
+                return self.record_length
+
+            try:
+                int(self.record_length)
+            except ValueError:
+                print('Invalid value, please re-input.')
+                continue
+
+            if int(self.record_length) < 0: 
+                print('Record must hold at least 1 historical result.')
+                continue
+
+            # Input is valid at this point
+            return self.record_length
+    
+    def _PromptContinue(self):
+
+        while True:
+            self.user_continue = input('Do you wish to continue? (Y/N) ')
+            if not self.user_continue:
+                self.running = 1
+                return self.running
+ 
+            if self.user_continue != 'Y' and self.user_continue != 'N':
+                print('Invalid value, please re-input.')
+                continue
+
+            if self.user_continue == 'N':
+                print('Thanks for playing!')
+                self.running = 0
+                return self.running
+
+            # user_continue is 'Y'
+            self.running = 1
+            return self.running
 
     def _DisplayState(self):
         print('Simulating a', self.max_roll, 'sided dice ...')
@@ -39,7 +105,7 @@ class DiceClass():
         print(self.result_list)
 
     def _DisplayAverage(self):
-        print('Last 10 average:',sum(self.result_list)/float(len(self.result_list)))
+        print('Average of last,' self.record_length, 'results:',sum(self.result_list)/float(len(self.result_list)))
 
     def _ResetRound(self):
         self.round_count= 0
@@ -50,8 +116,8 @@ class DiceClass():
         self.round_count += 1
         self.result = random.randint(int(self.min_roll), int(self.max_roll))
         print('Round', self.round_count, '- rolled', self.result)
-        if len(self.result_list) == int(self.max_record_length):
-            self.result_list.pop(0)  # Remove first item from list
+        if len(self.result_list) == int(self.record_length):
+            self.result_list.pop(0)
         self.result_list.append(self.result)
         return self.result
 
@@ -61,23 +127,17 @@ if __name__ == '__main__':
     x = DiceClass()
 
     # User input for initial state
-    x._SetState()
+    x._PromptMinRoll()
+    x._PromptMaxRoll()
+    x._PromptRecordLength()
     x._DisplayState()
 
-    # Game loop
-    while True:
+    # Game loop, x.running init with 1
+    while x.running:
         x._RollDice()
         x._DisplayRecords()
-        
-        # For Python3+ raw_input() has been renamed to input()
-        user_input = input('Do you wish to continue? (Y/N) [Y] ')
 
-        # If user input is empty, set to Y 
-        if not user_input:
-            user_input = 'Y'
- 
-        if user_input == 'N':
-            break
-        elif user_input != 'Y':
-            print('Invalid input, exiting.')
-            break
+        # PromptContinue() will change flag x.running based on user input
+        x._PromptContinue()
+            
+    print('Execution complete.')
