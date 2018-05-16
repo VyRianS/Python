43b1c12452cc0023419d261e1ede63e5467a2e4e
Jianyu <vyrians@gmail.com>
2018-05-16 22:58:31 +0800

    ~pgen.py

--- ds/cls.py
+++ ds/cls.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 
 class Classifier:
 
-    def __init__(self, csvfile, kpoints, dist_type='euclidean'):
+    def __init__(self, csvfile, kvalue, dist_type='euclidean'):
         self.csvfile = csvfile       # csvfile to read from via pandas
-        self.kpoints = kpoints       # k nearest points
+        self.kvalue = kvalue        # k nearest points
         self.dist_type = dist_type   # either Euclidean distance, or Hamming
         self._read()
         self._sort()
@@ -27,7 +27,7 @@ class Classifier:
         elif self.dist_type == 'hamming':
             self.df['D-Hamming'] = (self.df['X']-self.df_p['X'].values).abs() + (self.df['Y']-self.df_p['Y'].values).abs()
             self.df_sort = self.df.sort_values('D-Hamming')
-        self.df_result = self.df_sort.head(self.kpoints+1).tail(self.kpoints)
+        self.df_result = self.df_sort.head(self.kvalue+1)
 
     def GetResultSet(self):
         return self.df_result
@@ -36,15 +36,20 @@ class Classifier:
         return self.df
 
     def GetClass(self):
-        self.class_result = self.df_result['Class'].value_counts().idxmax()
+        self.class_result = self.df_result['Class'].tail(self.kvalue).value_counts().idxmax()
         return self.class_result
+
+    def GetDistType(self):
+        return self.dist_type
+
+    def GetKValue(self):
+        return self.kvalue
 
 if __name__ == '__main__':
 
-    cls = Classifier(csvfile='data.csv', kpoints=5, dist_type='hamming')
-    print(cls.GetClass())
+    cls = Classifier(csvfile='testdata.csv', kvalue=10, dist_type='euclidean')
+    print('?-point classified as:', cls.GetClass())
     print(cls.GetResultSet())
-
 
     '''
     # Visualize
--- ds/pgen.py
+++ ds/pgen.py
@@ -2,15 +2,15 @@
 
 # PGEN.PY - Point generator
 
+import argparse
 import csv
 import random
 import matplotlib.pyplot as plt
 
 # Generate a random list of points stored as series of 3-tuples
 # (category, x-coord, y-coord)
-# Use PANDAS maybe?
 
-class PointGen:
+class PointGenerator:
 
     def __init__(self,
                  category='A',
@@ -46,18 +46,53 @@ class PointGen:
 
 if __name__ == '__main__':
 
-    A = PointGen(category='A', npoints=10, min_x=0.4, min_y=0.3, max_x=0.8, max_y=1.)
+    # Create parent parser
+    parser = argparse.ArgumentParser(description='Generate a series of N randomized 2-D points with minimum and maximum boundaries.')
+    parser.add_argument('-f', '--csvfile', help='Write points into CSV file.')
+
+    # Create new argument group for compulsory arguments
+    argsRequired = parser.add_argument_group('required named arguments')
+    argsRequired.add_argument('-c', '--category', required=True, help='Data category, accepts any unique string.')
+    argsRequired.add_argument('-n', '--nvalue', required=True, help='Number of random points.')
+    argsRequired.add_argument('-x', '--xbounds', required=True, help='Coordinates in format: min_x,max_x.')
+    argsRequired.add_argument('-y', '--ybounds', required=True, help='Coordinates in format: min_y,max_y.')
+
+    # Initialize parser
+    args = parser.parse_args()
+
+    # Process input variables
+    arg_min_x, arg_max_x = args.xbounds.split(',')
+    arg_min_y, arg_max_y = args.ybounds.split(',')
+
+    '''
+    print('lower X:', arg_min_x)
+    print('lower Y:', arg_min_y)
+    print('upper X:', arg_max_x)
+    print('upper Y:', arg_max_y)
+    '''
+
+    # Create point generator class
+    pgen = PointGenerator(category=args.category, npoints=int(args.nvalue), min_x=float(arg_min_x), min_y=float(arg_min_y), max_x=float(arg_max_x), max_y=float(arg_max_y))
+
+    # Add points a csv file
+    with open(args.csvfile, 'a') as f:
+        cw = csv.writer(f, delimiter=',')
+        for coord in pgen.GetPoints():
+            cw.writerow(list(coord))
+
+    '''
+    A = PointGenerator(category='A', npoints=20, min_x=0.4, min_y=0.3, max_x=0.8, max_y=1.)
     map_A = A.GetPoints()
     x_A = A.GetX()
     y_A = A.GetY()
 
-    B = PointGen(category='B', npoints=10, min_x=0.1, min_y=0.2, max_x=0.6, max_y=0.6)
+    B = PointGenerator(category='B', npoints=20, min_x=0.1, min_y=0.2, max_x=0.6, max_y=0.6)
     map_B = B.GetPoints()
     x_B = B.GetX()
     y_B = B.GetY()
 
     # Our confused point!
-    P = PointGen(category='?', npoints=1, min_x=0.35, min_y=0.35, max_x=0.65, max_y=0.65)
+    P = PointGenerator(category='?', npoints=1, min_x=0.35, min_y=0.35, max_x=0.65, max_y=0.65)
     map_P = P.GetPoints()
     x_P = P.GetX()
     y_P = P.GetY()
@@ -80,4 +115,4 @@ if __name__ == '__main__':
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
-
+    '''
