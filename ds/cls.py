deed7f04a21e247d1468d429c65c3a6a834d5b45
Jianyu <vyrians@gmail.com>
2018-05-17 20:24:44 +0800

    ++pgen-circ.py

--- ds/cls.py
+++ ds/cls.py
@@ -3,32 +3,44 @@
 # CLS.PY - Classifer
 # let classifier read from a csv file?
 
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class Classifier:
+class Cls:
 
-    def __init__(self, csvfile, kvalue, dist_type='euclidean'):
+    def __init__(self, csvfile, category, kvalue, dist_type='euclidean'):
         self.csvfile = csvfile       # csvfile to read from via pandas
-        self.kvalue = kvalue        # k nearest points
+        self.category = category     # Point category to classify
+        self.kvalue = kvalue         # k nearest points
         self.dist_type = dist_type   # either Euclidean distance, or Hamming
         self._read()
         self._sort()
 
     def _read(self):
         self.df = pd.read_csv(self.csvfile)
-        self.df.columns = ['Class','X','Y'] 
-        self.df_p = self.df[self.df['Class'] == '?']
+        self.df.columns = ['Category','X','Y'] 
+        self.df_cat_unique = self.df['Category'].unique()
+
+        # Store data set in individual dataframe
+        self.DATA = {}
+        for i in range(len(self.df_cat_unique)):
+             self.DATA[self.df_cat_unique[i]] = self.df[self.df['Category'] == self.df_cat_unique[i]]
 
     # TODO: Fails to work when ?-record is at the top of the data file
     def _sort(self):
         if self.dist_type == 'euclidean':
-            self.df['D-Euclidean'] = ((self.df['X']-self.df_p['X'].values).pow(2) + (self.df['Y']-self.df_p['Y'].values).pow(2)).pow(0.5)
+            self.df['D-Euclidean'] = ((self.df['X'] - self.DATA[self.category]['X'].values).pow(2) + (self.df['Y'] - self.DATA[self.category]['Y'].values).pow(2)).pow(0.5)
             self.df_sort = self.df.sort_values('D-Euclidean')
         elif self.dist_type == 'hamming':
-            self.df['D-Hamming'] = (self.df['X']-self.df_p['X'].values).abs() + (self.df['Y']-self.df_p['Y'].values).abs()
+            self.df['D-Hamming'] = (self.df['X'] - self.DATA[self.category]['X'].values).abs() + (self.df['Y'] - self.DATA[self.category]['Y'].values).abs()
             self.df_sort = self.df.sort_values('D-Hamming')
-        self.df_result = self.df_sort.head(self.kvalue+1)
+        self.df_result = self.df_sort.head(self.kvalue+1)  # returns confused point as well
+
+    def GetPlot(self):
+        for dkey in self.DATA.keys():
+            plt.scatter(self.DATA[dkey]['X'], self.DATA[dkey]['Y'])
+        plt.show()
 
     def GetResultSet(self):
         return self.df_result
@@ -36,9 +48,9 @@ class Classifier:
     def GetDataSet(self):
         return self.df
 
-    def GetClass(self):
-        self.class_result = self.df_result['Class'].tail(self.kvalue).value_counts().idxmax()
-        return self.class_result
+    def GetCategory(self):
+        self.cat_result = self.df_result['Category'].tail(self.kvalue).value_counts().idxmax()
+        return self.cat_result
 
     def GetDistType(self):
         return self.dist_type
@@ -48,14 +60,34 @@ class Classifier:
 
 if __name__ == '__main__':
 
-    cls = Classifier(csvfile='data.csv', kvalue=10, dist_type='euclidean')
-    print('?-point classified as:', cls.GetClass())
-    print(cls.GetResultSet())
+    # Create parent parser
+    parser = argparse.ArgumentParser(description='Classifies orphan point by calculating k-nearest-neighbors.')
 
-    '''
-    # Visualize
-    plt.scatter(df_A['X'], df_A['Y'])
-    plt.scatter(df_B['X'], df_B['Y'])
-    plt.scatter(df_P['X'], df_P['Y'])
-    plt.show()
-    '''
+    # Create new argument group for compulsory arguments
+    argsRequired = parser.add_argument_group('required named arguments')
+    argsRequired.add_argument('-f', '--csvfile', required=True,
+                              help='Reads dataset from CSV file.')
+    argsRequired.add_argument('-c', '--category', required=True,
+                              help='Data category of unclassified point.')
+    argsRequired.add_argument('-k', '--kvalue', required=True,
+                              help='Classify point according to K-nearest neighbors.')
+    argsRequired.add_argument('-d', '--dist-type', required=True,
+                              choices=['euclidean','hamming'],
+                              help='Distance measuring algorithm.')
+
+    # Initialize parser
+    args = parser.parse_args()
+
+    # Process input variables
+    int_kvalue = int(args.kvalue)
+
+    # Create classifier instance
+    cls = Cls(csvfile=args.csvfile,
+              category=args.category,
+              kvalue=int_kvalue,
+              dist_type=args.dist_type)
+
+    print('Point classified as:', cls.GetCategory())
+    print(cls.GetResultSet())
+    cls.GetPlot()
+
--- ds/pgen-circ.py
+++ ds/pgen-circ.py
@@ -5,29 +5,33 @@
 import argparse
 import csv
 import random
+import math
 import matplotlib.pyplot as plt
 
 # Generate a random list of points stored as series of 3-tuples
 # (category, x-coord, y-coord)
 
-class PointGeneratorRect:
+class PointGeneratorCirc:
 
     def __init__(self, category, npoints,
-                 min_x, min_y,
-                 max_x, max_y):
+                 origin_x,
+                 origin_y,
+                 radius):
         self.category = category
         self.npoints = npoints
-        self.min_x = min_x
-        self.min_y = min_y
-        self.max_x = max_x
-        self.max_y = max_y
+        self.or_x = origin_x
+        self.or_y = origin_y
+        self.radius = radius
         self.POINTS = []
         self._Generate()
 
     def _Generate(self):
         for i in range(self.npoints):
-            rand_x = random.uniform(self.min_x, self.max_x)
-            rand_y = random.uniform(self.min_y, self.max_y)
+            # need to divide the upper radius bounds by 2, WHY???
+            rand_r = random.uniform(0, self.radius/2)
+            rand_a = random.uniform(0, math.pi*2)
+            rand_x = self.or_x + rand_r**0.5 * math.cos(rand_a)
+            rand_y = self.or_y + rand_r**0.5 * math.sin(rand_a)
             self.POINTS.append((self.category, rand_x, rand_y))
         self.POINTS_X = [x[1] for x in self.POINTS]
         self.POINTS_Y = [x[2] for x in self.POINTS]
@@ -45,7 +49,7 @@ class PointGeneratorRect:
 if __name__ == '__main__':
 
     # Create parent parser
-    parser = argparse.ArgumentParser(description='Generate a series of N randomized 2-D points in a rectangular area within minimum and maximum X and Y coordinates.')
+    parser = argparse.ArgumentParser(description='Generate a series of N randomized 2-D points in a circular area given origin and maximum disk radius.')
     parser.add_argument('-p', '--plot', action='store_true',
                         help='Plots generated dataset.')
     parser.add_argument('-f', '--csvfile', help='Appends data into CSV file.')
@@ -56,28 +60,26 @@ if __name__ == '__main__':
                               help='Data category, accepts any unique string.')
     argsRequired.add_argument('-n', '--nvalue', required=True,
                               help='Number of random points.')
-    argsRequired.add_argument('-x', '--xbounds', required=True,
-                              help='Coordinates in format: min_x,max_x.')
-    argsRequired.add_argument('-y', '--ybounds', required=True,
-                              help='Coordinates in format: min_y,max_y.')
+    argsRequired.add_argument('-o', '--origin', required=True,
+                              help='Coordinates in format: origin_x,origin_y.')
+    argsRequired.add_argument('-r', '--radius', required=True,
+                              help='Maximum radius of circle.')
 
     # Initialize parser
     args = parser.parse_args()
 
     # Process input variables
     int_nvalue = int(args.nvalue)
-    flt_min_x = float(args.xbounds.split(',')[0])
-    flt_max_x = float(args.xbounds.split(',')[1])
-    flt_min_y = float(args.ybounds.split(',')[0])
-    flt_max_y = float(args.ybounds.split(',')[1])
+    flt_or_x = float(args.origin.split(',')[0])
+    flt_or_y = float(args.origin.split(',')[1])
+    flt_radius = float(args.radius)
 
     # Create point generator instance
-    pgen = PointGeneratorRect(category=args.category, 
+    pgen = PointGeneratorCirc(category=args.category, 
                               npoints=int_nvalue, 
-                              min_x=flt_min_x, 
-                              min_y=flt_min_y, 
-                              max_x=flt_max_x, 
-                              max_y=flt_max_y)
+                              origin_x = flt_or_x,
+                              origin_y = flt_or_y,
+                              radius = flt_radius)
 
     # Add points a csv file
     if args.csvfile:
